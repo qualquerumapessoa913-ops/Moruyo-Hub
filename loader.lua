6cb5811786fc39524d0b39aad5ff5b912a3572b3
Pietro Martins Ramalho <qualquerumapessoa913@gmail.com>
2026-09-05 16:33:07 -0300

    Commit Changes

--- loader.lua
+++ loader.lua
@@ -4,6 +4,10 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
--- Carrega o main.lua (que já carrega todos os módulos)
+-- Substitua pelo link RAW correto
 local main = loadstring(game:HttpGet("https://raw.githubusercontent.com/qualquerumapessoa913-ops/MoruyoHub/main/src/main.lua"))()
-main()
+if main then
+    main()
+else
+    warn("Falha ao carregar o hub!")
+end
